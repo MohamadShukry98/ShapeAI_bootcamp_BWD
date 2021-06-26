bc10c0a533fee25669de3bf070a3b2f50911d818
Mohamad Shukry <86024149+MohamadShukry98@users.noreply.github.com>
2021-06-26 22:49:52 +0530

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> javascript and React.js</h1>
+      <p> This was an amazing bootcamp taken up by shaurya shinha we covered everything from Scratch including Java script React.js, HTML..</p>
+    </div>
+  );
+}
+
+export default Notes;
